c3cb77d595071a27f118ebf719c6ed03db3e6063
Michael Leighton <michael138leighton@gmail.com>
2022-08-19 23:08:43 -0500

    file can be added to database

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-require('dotenv').config
+require('dotenv').config()
 const multer = require('multer')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const upload = multer({dest: 'uploads'})
+const bcrypt = require('bcrypt')
+const file = require('./models/file')
+const File = require('./models/file')
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -14,8 +17,18 @@ app.get('/', (req, res)=>{
     res.render('index')
 })
 
-app.post('/upload', upload.single('file'), (req, res)=>{
-    res.send('hi')
+app.post('/upload', upload.single('file'), async (req, res)=>{
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname, 
+    }
+    if(req.body.password != '' && req.body.password != null){
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData)
+    console.log(file)
+    res.send(file.originalName)
 })
 
 app.listen(process.env.PORT)
